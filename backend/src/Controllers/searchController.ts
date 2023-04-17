9ed8bbf21b064a5a30c135239bbbd9af4a68b102
Rafael Aguiar <terule@gmail.com>
2023-04-17 08:22:24 -0300

    feat: 🦾 Searches controller layer

---
+++ backend/src/Controllers/searchController.ts
@@ -0,0 +1,42 @@
+import { type Request, type Response } from 'express';
+import type ISearch from '../Interfaces/searchInterface';
+import { deleteSearchService, getSearchService, saveSearchService } from '../Services/searchService';
+import { getProducts } from '../Utils/getProducts';
+import { sorter } from '../Utils/sorter';
+
+const searchController = async (req: Request, res: Response): Promise<void> => {
+  const { web, query, category } = req.body as ISearch;
+  if (!web || !query || !category) {
+    res.status(400).json({ message: 'Bad Request' });
+    return;
+  }
+  const scrapedProducts = await getProducts(web, query);
+  const databaseProducts = await getSearchService(web, category);
+
+  // If there are no products in the database, save the scraped products
+  if (databaseProducts.length === 0) {
+    await saveSearchService(scrapedProducts, web, category);
+    res.status(200).json({ products: scrapedProducts });
+    return;
+  }
+
+  // If there are products in the database, compare them with the scraped products
+  const sortedProducts = sorter(scrapedProducts, databaseProducts);
+
+  // If there are new products, save them
+  if (sortedProducts.newProducts.length > 0) {
+    await saveSearchService(sortedProducts.newProducts, web, category);
+  }
+
+  // If there are deleted products, delete them
+  if (sortedProducts.deletedProducts.length > 0) {
+    await deleteSearchService(sortedProducts.deletedProducts);
+  }
+
+  res.status(200).json({
+    products: [...sortedProducts.newProducts, ...sortedProducts.existingProducts],
+    deletedPRoducts: sortedProducts.deletedProducts
+  });
+};
+
+export { searchController };
--- backend/src/Routes/searchRoute.ts
+++ backend/src/Routes/searchRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { searchController } from '../Controller/searchController';
+import { searchController } from '../Controllers/searchController';
 
 const router = Router();
 
